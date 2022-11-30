6fcc2c56997c267d536229196493fbd2bc408787
Elisandro <Elisandro@LAPTOP-CCH60LK6>
2022-11-30 19:13:24 -0300

    Adicionar arquivos de projeto.

---
+++ FormulárioWindowsForm/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace FormulárioWindowsForm
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(143, 65);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(333, 22);
+            this.txtNome.TabIndex = 0;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(143, 101);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(333, 22);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(66, 65);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(44, 16);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "Nome";
+            this.lblNome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(65, 104);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(45, 16);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "E-mail";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(65, 141);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(61, 16);
+            this.lblTelefone.TabIndex = 4;
+            this.lblTelefone.Text = "Telefone";
+            this.lblTelefone.Click += new System.EventHandler(this.lblTelefone_Click);
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(143, 138);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(333, 22);
+            this.txtTelefone.TabIndex = 5;
+            this.txtTelefone.TextChanged += new System.EventHandler(this.txtTelefone_TextChanged);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(341, 198);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(135, 32);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 286);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form";
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ FormulárioWindowsForm/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace FormulárioWindowsForm
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTelefone_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTelefone_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //String conexão com banco de dados
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-CCH60LK6;Initial Catalog=teste;Integrated Security=True");
+
+            //String de inserção no banco de dados
+            string sql = "INSERT INTO pessoas (id, nome, email, telefone) VALUES (@id, @nome, @email, @telefone)";
+            Random numeroID = new Random();
+            numeroID.Next();
+
+            try
+            {
+                //Cria um objeto do tipo comando passando com parâmetros as strings sql e conn 
+                SqlCommand c = new SqlCommand(sql, conn);
+
+                //Insere os dados  da txtBox no comando sql
+                c.Parameters.Add(new SqlParameter("@id", numeroID.Next()));
+                c.Parameters.Add(new SqlParameter("@nome", this.txtNome.Text));
+                c.Parameters.Add(new SqlParameter("@email", this.txtEmail.Text));
+                c.Parameters.Add(new SqlParameter("@telefone", this.txtTelefone.Text));
+
+                //Abre a conexão com o banco
+                conn.Open();
+
+                //Executa o comando Sql
+                c.ExecuteNonQuery();
+
+                //Fecha a conexão com o banco
+                conn.Close();
+
+                MessageBox.Show("Registro salvo com sucesso!");
+
+            } catch (SqlException ex) 
+            { 
+                MessageBox.Show("Ocorreu o erro de conexão: " + ex);
+            }
+            finally
+            { 
+                conn.Close(); 
+            }
+        }
+    }
+}
